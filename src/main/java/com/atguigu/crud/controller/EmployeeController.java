fbc25311e19098d8accb6bd5a9eaed2d8e460099
zhulinjia <695157144@qq.com>
2022-08-16 17:22:06 +0800

    day04  22/8/16  完成表单校验

--- src/main/java/com/atguigu/crud/controller/EmployeeController.java
+++ src/main/java/com/atguigu/crud/controller/EmployeeController.java
@@ -25,38 +25,81 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
+    /**
+     * @作者: zhulinjia
+     * @时间: 2022/8/16 14:38
+     * @Return:
+     * @Trans:
+     */
+
+    /*@RequestMapping("/checkuser")
+    @ResponseBody
+    public Msg checkUser(@RequestParam("empName") String empName) {
+        // 先判断用户名是否是合法的表达式
+        String regx = "(^[a-zA-Z0-9_-]{6,16}$)|(^[\\u2E80-\\u9FFF]{2,5})";
+        // 检查正则表达式是否匹配
+         if(!empName.matches(regx)){
+              return Msg.fail().add("va_msg","用户名必须是6-16位数字和字母的组合");
+         }
+
+        boolean b = employeeService.checkUser(empName);
+
+        if(b){
+            return Msg.success();
+        }else{
+            return Msg.fail().add("va_msg","用户名不可用");
+        }
+
+    }*/
+
+
+    @ResponseBody
+    @RequestMapping("/checkuser")
+    public Msg checkuser(@RequestParam("empName")String empName){
+        //先判断用户名是否是合法的表达式;
+        String regx = "(^[a-zA-Z0-9_-]{6,16}$)|(^[\u2E80-\u9FFF]{2,5})";
+        if(!empName.matches(regx)){
+            return Msg.fail().add("va_msg", "用户名必须是6-16位数字和字母的组合或者2-5位中文");
+        }
+
+        //数据库用户名重复校验
+        boolean b = employeeService.checkUser(empName);
+        if(b){
+            return Msg.success();
+        }else{
+            return Msg.fail().add("va_msg", "用户名不可用");
+        }
+    }
+
+
+
     //员工保存
-    @RequestMapping(value = "/emp",method = RequestMethod.POST)
+    @RequestMapping(value = "/emp", method = RequestMethod.POST)
     @ResponseBody
     // 页面传输的数据和参数一致的情况下会自动封装
-    public Msg saveEmp(Employee employee){
+    public Msg saveEmp(Employee employee) {
         employeeService.saveEmp(employee);
 
         return Msg.success();
     }
 
 
-
     @RequestMapping("/emps")
     @ResponseBody
-   public Msg getEmpWithJson(@RequestParam(value = "pn", defaultValue = "1") Integer pn,
-                             Model model) {
+    public Msg getEmpWithJson(@RequestParam(value = "pn", defaultValue = "1") Integer pn,
+                              Model model) {
 
-       // 这不是一个分页查询；
-       // 引入PageHelper分页插件
-       // 在查询之前只需要调用，传入页码，以及每页的大小
-       PageHelper.startPage(pn, 5);
-       // startPage后面紧跟的这个查询就是一个分页查询
-       List<Employee> emps = employeeService.getAll();
-       // 使用pageInfo包装查询后的结果，只需要将pageInfo交给页面就行了。
-       // 封装了详细的分页信息,包括有我们查询出来的数据，传入连续显示的页数
-       PageInfo page = new PageInfo(emps, 5);
-       return Msg.success().add("pageInfo",page);
-   }
-
-
-
-
+        // 这不是一个分页查询；
+        // 引入PageHelper分页插件
+        // 在查询之前只需要调用，传入页码，以及每页的大小
+        PageHelper.startPage(pn, 5);
+        // startPage后面紧跟的这个查询就是一个分页查询
+        List<Employee> emps = employeeService.getAll();
+        // 使用pageInfo包装查询后的结果，只需要将pageInfo交给页面就行了。
+        // 封装了详细的分页信息,包括有我们查询出来的数据，传入连续显示的页数
+        PageInfo page = new PageInfo(emps, 5);
+        return Msg.success().add("pageInfo", page);
+    }
 
 
     /**
@@ -64,7 +107,7 @@ public class EmployeeController {
      *
      * @return
      */
-   // @RequestMapping("/emps")
+    // @RequestMapping("/emps")
     public String getEmps(
             @RequestParam(value = "pn", defaultValue = "1") Integer pn,
             Model model) {
--- src/main/java/com/atguigu/crud/service/EmployeeService.java
+++ src/main/java/com/atguigu/crud/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.atguigu.crud.service;
 
 import com.atguigu.crud.bean.Employee;
+import com.atguigu.crud.bean.EmployeeExample;
 import com.atguigu.crud.dao.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,22 @@ public class EmployeeService {
 
     @Autowired
     EmployeeMapper employeeMapper;
+
+    /**
+     * @作者: zhulinjia
+     * @时间: 2022/8/16 14:41
+     * @Return: 返回 true 代表当前姓名可以用  返回false 代表用户名不可用
+     * @Trans:  用户名
+     * 校验用户姓名是否可用
+     */
+    public boolean checkUser(String empName){
+        EmployeeExample example = new EmployeeExample();
+        EmployeeExample.Criteria criteria = example.createCriteria();
+        criteria.andEmpNameEqualTo(empName);
+        long count = employeeMapper.countByExample(example);
+        return count==0;
+    }
+
 
     /**
      * 查询所有员工
